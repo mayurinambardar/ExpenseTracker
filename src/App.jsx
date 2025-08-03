48dc3ae2e839bb530213c604a898af15c88d902c
mayurinambardar <71820138+mayurinambardar@users.noreply.github.com>
2025-08-03 18:18:37 +0000

    removed test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import AddExpenseForm from "./components/AddExpenseForm";
 import ExpenseList from "./components/ExpenseList";
-import Test from "./components/Test";
 function App() {
   return (
     <>
